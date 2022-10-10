a5e5dff117c754841a01ff3dad40278961ec9fe5
LucasKienast <lucas.kienast@hotmail.de>
2022-10-10 09:51:24 +0100

    jc-2: megabyte converter

--- src/com/kienast/Main.java
+++ src/com/kienast/Main.java
@@ -1,5 +1,6 @@
 package com.kienast;
 
+import static com.kienast.megabytesconverter.MegaBytesConverter.printMegaBytesAndKiloBytes;
 import static com.kienast.speedconverter.SpeedConverter.printConversion;
 import static com.kienast.speedconverter.SpeedConverter.toMilesPerHour;
 
@@ -7,8 +8,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Speed converter
+        // Q1: Speed converter
+        /*
         System.out.println("100 km/h = " + toMilesPerHour(100d) + " mi/h");
         printConversion(100d);
+         */
+
+
+        // Q2: Megabytes converter
+        // printMegaBytesAndKiloBytes(2500);
+
+
     }
 }
