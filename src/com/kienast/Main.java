cb63a3ebbd98f7a4c866c964c052891b12ab5912
LucasKienast <lucas.kienast@hotmail.de>
2022-10-10 09:32:28 +0100

    jc-1: speedconverter

--- src/com/kienast/Main.java
+++ src/com/kienast/Main.java
@@ -1,8 +1,14 @@
 package com.kienast;
 
+import static com.kienast.speedconverter.SpeedConverter.printConversion;
+import static com.kienast.speedconverter.SpeedConverter.toMilesPerHour;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Speed converter
+        System.out.println("100 km/h = " + toMilesPerHour(100d) + " mi/h");
+        printConversion(100d);
     }
 }
