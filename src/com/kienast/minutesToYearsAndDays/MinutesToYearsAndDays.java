f27ffb466c98e17c2cc5e87475ad0ff118e281d2
LucasKienast <lucas.kienast@hotmail.de>
2022-10-10 15:09:10 +0100

    jc-8: minutes to years and days

---
+++ src/com/kienast/minutesToYearsAndDays/MinutesToYearsAndDays.java
@@ -0,0 +1,19 @@
+package com.kienast.minutesToYearsAndDays;
+
+public class MinutesToYearsAndDays {
+
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid input");
+            return;
+        }
+
+        long totalHours = minutes / 60;
+        long totalDays = totalHours / 24;
+        long totalYears = totalDays / 365;
+        long residueDays = totalDays % 365;
+
+        System.out.println(minutes + " min = " + totalYears + " years and " + residueDays + " days");
+    }
+
+}
