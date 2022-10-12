f0dd0b7bcde68f6cfb8e943e6519491fd9467234
LucasKienast <lucas.kienast@hotmail.de>
2022-10-12 15:29:25 +0100

    jc-17: last digit checker

---
+++ src/com/kienast/lastDigitChecker/LastDigitChecker.java
@@ -0,0 +1,24 @@
+package com.kienast.lastDigitChecker;
+
+public class LastDigitChecker {
+
+    public static boolean hasSameLastDigit(int a, int b, int c) {
+        if ((a < 10) || (b < 10) || (c < 10) || (a >1000) || (b > 1000) || (c > 1000)) {
+            System.out.println("Invalid input");
+            return false;
+        }
+
+        int lastDigitA = a % 10;
+        int lastDigitB = b % 10;
+        int lastDigitC = c % 10;
+
+        if ((lastDigitA == lastDigitB) || (lastDigitA == lastDigitC) || (lastDigitB == lastDigitC)) {
+            System.out.println("Two numbers have the same last digit");
+            return true;
+        }
+
+        System.out.println("Two numbers do not have the same last digit");
+        return false;
+    }
+
+}
