816821585fe8706982b34337f66b51e9694ffa57
LucasKienast <lucas.kienast@hotmail.de>
2022-10-20 11:05:02 +0100

    jc-42: saveable

---
+++ src/com/kienast/interfaces/Player.java
@@ -0,0 +1,81 @@
+package com.kienast.interfaces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player implements ISaveable {
+
+    private String name;
+    private int hitPoints;
+    private int strength;
+    private String weapon;
+
+    public Player(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+        this.weapon = "Sword";
+    }
+
+    @Override
+    public List<String> write() {
+        List<String> fields = new ArrayList<>();
+        fields.add(0, name);
+        fields.add(1, String.valueOf(hitPoints));
+        fields.add(2, String.valueOf(strength));
+        fields.add(3, weapon);
+        return fields;
+    }
+
+    @Override
+    public void read(List<String> savedValues) {
+        if (savedValues != null && savedValues.size() > 0) {
+            name = savedValues.get(0);
+            hitPoints = Integer.parseInt(savedValues.get(1));
+            strength = Integer.parseInt(savedValues.get(2));
+            weapon = savedValues.get(3);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", strength=" + strength +
+                ", weapon='" + weapon + '\'' +
+                '}';
+    }
+}
