e2fd2a244ea8ba413e8b45f276006564149cc967
LucasKienast <lucas.kienast@hotmail.de>
2022-10-22 13:48:30 +0100

    jc-45: generics league

--- src/com/kienast/Main.java
+++ src/com/kienast/Main.java
@@ -5,6 +5,7 @@ import com.kienast.abstractClass.Node;
 import com.kienast.abstractClass.SearchTree;
 import com.kienast.arrayList.MobilePhone;
 import com.kienast.autoboxing.Bank;
+import com.kienast.generics.*;
 import com.kienast.interfaces.ISaveable;
 import com.kienast.interfaces.Monster;
 import com.kienast.interfaces.Player;
@@ -511,6 +512,7 @@ public class Main {
 
 
         // Q45: Abstract class
+        /*
         // MyList list = new MyList(null);
         SearchTree tree = new SearchTree(null);
         tree.traverse(tree.getRoot());
@@ -547,6 +549,47 @@ public class Main {
         tree.traverse(tree.getRoot());
         tree.removeItem(tree.getRoot());
         tree.traverse(tree.getRoot());
+         */
+
+
+        // Q46: Generics for league table
+        League<Team<FootballPlayer>> footballLeague = new League<>("AFL");
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        Team<FootballPlayer> hawthorn= new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle= new Team<>("Fremantle");
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+
+        hawthorn.matchResult(fremantle, 1, 0);
+        hawthorn.matchResult(adelaideCrows, 3, 8);
+
+        adelaideCrows.matchResult(fremantle, 2, 1);
+
+        footballLeague.add(adelaideCrows);
+        footballLeague.add(melbourne);
+        footballLeague.add(hawthorn);
+        footballLeague.add(fremantle);
+
+//        footballLeague.add(baseballTeam);
+        footballLeague.showLeagueTable();
+
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+        Team rawTeam = new Team("Raw Team");
+        rawTeam.addPlayer(beckham); // unchecked warning
+        rawTeam.addPlayer(pat);     // unchecked warning
+
+        footballLeague.add(rawTeam);     // unchecked warning
+
+        League<Team> rawLeague = new League<>("Raw");
+        rawLeague.add(adelaideCrows);     // no warning
+        rawLeague.add(baseballTeam);    // no warning
+        rawLeague.add(rawTeam);         // no warning
+
+        League reallyRaw = new League("Really raw");
+        reallyRaw.add(adelaideCrows);     // unchecked warning
+        reallyRaw.add(baseballTeam);    // unchecked warning
+        reallyRaw.add(rawTeam);         // unchecked warning
     }
 
     /*
---
+++ src/com/kienast/generics/SoccerPlayer.java
@@ -0,0 +1,7 @@
+package com.kienast.generics;
+
+public class SoccerPlayer extends Player {
+    public SoccerPlayer(String name) {
+        super(name);
+    }
+}
